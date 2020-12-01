15240cd6bf54c740f4a7c94597d7463464146ed9
jamoreno22 <javier.morenog@sansano.usm.cl>
2020-12-01 17:22:57 -0300

    dristibute chunks fixed

--- server/server.go
+++ server/server.go
@@ -105,7 +105,7 @@ func runSendProposal(nc data.NameNodeClient, proposals []data.Proposal) error {
 func (d *dataNodeServer) DistributeChunks(ctx context.Context, req *data.Chunk) (*data.Message, error) {
 	os.Open("Chunks/")
 	ioutil.WriteFile(req.Name, req.Data, os.ModeAppend)
-	return &data.Message{Text: "OwO"}, nil
+	return nil, nil
 }
 
 //SendBookInfo
@@ -123,24 +123,21 @@ func (d *dataNodeServer) DistributionType(ctx context.Context, req *data.Message
 
 // DistributeChunks in another datanodes
 func runDistributeChunks(props []data.Proposal) error {
+	//-----  crear las conexiones a los otros datanodes ----------------------
+	var datanode1Conn *grpc.ClientConn
+	datanode1Conn, err2 := grpc.Dial("10.10.28.17:9000", grpc.WithInsecure())
+	if err2 != nil {
+		log.Fatalf("did not connect: %s", err2)
+	}
+
+	// Datanode_3 Connection -------------------------------------------
+	var datanode3Conn *grpc.ClientConn
+	datanode3Conn, err3 := grpc.Dial("10.10.28.19:9000", grpc.WithInsecure())
+	if err3 != nil {
+		log.Fatalf("did not connect: %s", err3)
+	}
+
 	for _, prop := range props {
-
-		//-----  crear las conexiones a los otros datanodes ----------------------
-		var datanode1Conn *grpc.ClientConn
-		datanode1Conn, err2 := grpc.Dial("10.10.28.17:9000", grpc.WithInsecure())
-		if err2 != nil {
-			log.Fatalf("did not connect: %s", err2)
-		}
-		defer datanode1Conn.Close()
-
-		// Datanode_3 Connection -------------------------------------------
-		var datanode3Conn *grpc.ClientConn
-		datanode3Conn, err3 := grpc.Dial("10.10.28.19:9000", grpc.WithInsecure())
-		if err3 != nil {
-			log.Fatalf("did not connect: %s", err3)
-		}
-		defer datanode3Conn.Close()
-
 		if prop.Ip == "10.10.28.18:9000" {
 			// write/save buffer to disk
 			os.Open("Chunks/")
@@ -159,6 +156,8 @@ func runDistributeChunks(props []data.Proposal) error {
 			}
 		}
 	}
+	defer datanode1Conn.Close()
+	defer datanode3Conn.Close()
 	return nil
 }
 
